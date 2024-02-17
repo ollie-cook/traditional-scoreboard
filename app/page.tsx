bb2a2ddc949bee3327193926ba46f2c2fed08bfd
Oliver Cook <olliewatford@gmail.com>
2024-02-17 17:33:06 +0000

    forcing vercel redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { type Fixture } from '@/app/utils/types'
 import FormAndScoreBoard from './components/FormAndScoreBoard'
 
+//force redeployment
+
 export default async function Home() {
   const todaysFixtures = await getTodaysFixtures();
 
